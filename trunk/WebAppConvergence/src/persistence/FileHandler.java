8bbf251969c49bf9526c7f50353b0585958fe19f
yvensre@gmail.com <yvensre@gmail.com@750f5e98-d1c5-0636-b885-c7ff50f145bc>
2012-11-09 17:28:15 +0000

    Yvens
    Implementado oa leitura de arquivo da classe FileHandler e consequentemente, o seu primeiro teste já passou. Dê uma lida na documentação das classes para ver o que eu coloquei para cada uma que eu criei. Estou tentando comentar mais o código dessa vez.

--- trunk/WebAppConvergence/src/persistence/FileHandler.java
+++ trunk/WebAppConvergence/src/persistence/FileHandler.java
@@ -1,7 +1,17 @@
 package persistence;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Classe responsável pela leitura e alteração em arquivos
+ * 
+ * Inicialmente, a idéia é que os arquivos do asterisk serão carregados em disco 
+ * na máquina que estará fazendo as alterações, modificada e salva localmente, 
+ * e depois reenviada ao servidor.
  * 
  * @author yvens
  *
@@ -21,10 +31,47 @@ public class FileHandler {
 	 * 
 	 * @param path
 	 * @return um vetor de String através de um split em "/r/n" do arquivo lido
+	 * @throws IOException 
 	 */
-	public String[] readFile(String path)
+	public String[] readFile(String path) throws IOException
 	{
-		return null;
+		String text = "";
+		String file[] = null;
+		
+		FileInputStream fstream = null;
+		DataInputStream in = null;
+		BufferedReader br = null;
+		
+		try {
+			//Lê o arquivo que está no path
+			fstream = new FileInputStream(path);
+			
+			//Carrega as classes de leitura
+			in = new DataInputStream(fstream);
+			br = new BufferedReader(new InputStreamReader(in));
+			
+			//Instanciado uma String para a leitura linha a linha
+			String strLine;
+			while ((strLine = br.readLine()) != null) {
+				if(text == ""){
+					text += strLine;
+				}
+				else
+				{
+					text += "\r\n"+strLine;
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Erro: " + e.getMessage());
+		} finally {
+			br.close();
+			in.close();
+			fstream.close();
+		}
+		
+		file = text.split("\r\n");
+		
+		return file;
 	}
 	
 	/**
