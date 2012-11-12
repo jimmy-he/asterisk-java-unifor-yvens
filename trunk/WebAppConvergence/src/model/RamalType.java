044f841e3cdcca7fdc5e0efdf4644ac3c33b52ce
yvensre@gmail.com <yvensre@gmail.com@750f5e98-d1c5-0636-b885-c7ff50f145bc>
2012-11-12 18:52:43 +0000

    Yvens
    Adicionei os arquivos que o Geneflides disponibilizou sobre o trabalho.
    Implementei algumas funcionalidades no RamalType para poder pegar o enum de acordo com a String.
    Implementado mais um campo no Ramal correspondente ao campo accountCode do sip.conf (consta no segundo exemplo).
    Implementado a listagem dos ramais do RamalHandler. Os testes correspondentes passaram.

--- trunk/WebAppConvergence/src/model/RamalType.java
+++ trunk/WebAppConvergence/src/model/RamalType.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,14 +10,49 @@ import java.util.List;
  *
  */
 public enum RamalType {
+	FRIEND("FRIEND");
+	
+	private String type;
+	private RamalType(String type){
+		this.type = type;
+	}
+	
+	/**
+	 * Imprime o type do RamalType em um objeto do tipo String
+	 */
+	public String toString(){
+		return type;
+	}
 
-	FRIEND;
+	/**
+	 * Método para dado um type retornar o Enum do tipo passado
+	 * 
+	 * @param type
+	 * @return retorna um Enum RamalType que tenha o type passado, ou nulo caso não exista
+	 */
+	public static RamalType getRamalType(String type){
+		RamalType[] list = RamalType.class.getEnumConstants();
+		
+		for (int i = 0; i < list.length; i++) {
+			if(list[i].toString().equalsIgnoreCase(type)){
+				return list[i];
+			}
+		}
+		return null;
+	}
 	
 	/**
 	 * Método para retornar uma lista de Strings com o nome de todos os tipos de ramal
-	 * @return
+	 * @return retorna um ArrayList de Strings
 	 */
 	public static List<String> listRamalType(){
-		return null;
+		RamalType[] list = RamalType.class.getEnumConstants();
+		List<String> listRamalType = new ArrayList<String>();
+		
+		for (int i = 0; i < list.length; i++) {
+			listRamalType.add(list[i].toString());
+		}
+		
+		return listRamalType;
 	}
 }
