d88dda15d592ef2eeac527770ec03ffa8ba31cdc
santiagodsdp@gmail.com <santiagodsdp@gmail.com@750f5e98-d1c5-0636-b885-c7ff50f145bc>
2012-11-15 23:12:50 +0000

    Daniel   Colocando mais coisas pra testar em AsteriskSocketTest e atualizando o "manager e http atual" com como deve ficar o manager.conf

--- trunk/WebAppConvergence/src/tests/AsteriskSocketTest.java
+++ trunk/WebAppConvergence/src/tests/AsteriskSocketTest.java
@@ -16,50 +16,74 @@ public class AsteriskSocketTest {
 	public void initialAsteriskSocketTest() {
 		try {
 			AsteriskSocket socket = new AsteriskSocket();
-			
+
 			List<String> list = socket.receiveMessage();
-			for(String message : list){
+
+			for (String message : list) {
 				System.out.println(message);
 			}
-			
+
 			socket.delay(500);
-			
+
 			socket.sendMessage("Action: login\r\nUsername: root\r\nSecret: root\r\n\r\n");
-			
+
 			socket.delay(500);
-			
+
 			list = socket.receiveMessage();
-			for(String message : list){
+			for (String message : list) {
 				System.out.println(message);
 			}
-			
+
 			socket.sendMessage("Action: ping\r\n\r\n");
-			
+
 			socket.delay(500);
-			
+
 			list = socket.receiveMessage();
-			for(String message : list){
+			for (String message : list) {
+				System.out.println(message);
+			}
+
+			// se o user root no manager.conf não tiver todas as permissões
+			// possíveis, esse comando responderá "permission denied"
+
+			socket.sendMessage("Action: SIPpeers\r\n\r\n");
+
+			socket.delay(500);
+
+			list = socket.receiveMessage();
+			for (String message : list) {
 				System.out.println(message);
 			}
 			
-			socket.sendMessage("Action: ListCommands\r\n\r\n");
-			
+			socket.sendMessage("Action: IAXpeers\r\n\r\n");
+
 			socket.delay(500);
-			
+
 			list = socket.receiveMessage();
-			for(String message : list){
+			for (String message : list) {
 				System.out.println(message);
 			}
-			
+
+			// é igual ao service asterisk reload
+
+			socket.sendMessage("Action: reload\r\n\r\n");
+
+			socket.delay(500);
+
+			list = socket.receiveMessage();
+			for (String message : list) {
+				System.out.println(message);
+			}
+
 			socket.sendMessage("Action: Logoff\r\n\r\n");
-			
+
 			socket.delay(500);
-			
+
 			list = socket.receiveMessage();
-			for(String message : list){
+			for (String message : list) {
 				System.out.println(message);
 			}
-				
+
 			Assert.assertEquals(true, list != null && !list.isEmpty());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
